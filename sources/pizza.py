cd0bacf25bc9cfb91d837e7f1517ff9a4ea556ea
Vidalay CHANTHAVONG <vidalay@hotmail.com>
2021-09-27 13:54:14 +0200

    My new pizza

--- sources/pizza.py
+++ sources/pizza.py
@@ -35,5 +35,9 @@ RECIPES = {
     'mediterranea': {
         'ingredients': {'mozzarella': 3, 'pepper': 4, 'onion': 3},
         'price': 11,
+    },
+    'my_pizza': {
+        'ingredients': {'mozzarella': 3, 'pepper': 4},
+        'price': 11,
     }
 }
